8e0939b03546deaf4b2d81859bccf4f2fe7c8ad0
Raiza <wisdomadebola62@gmail.com>
2023-11-18 00:14:06 +0100

    Importing Fonts for Logo and Texts

--- Zena/tailwind.config.js
+++ Zena/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+          heading: ['Rubik', 'sans-serif'],
+          text: ['Rubik', 'sans-serif']
+        }
+    },
   },
   plugins: [],
 }
