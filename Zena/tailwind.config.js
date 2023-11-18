24bb5ab997b238f64b1cde36bfcd5d4a2c7d16c5
Raiza <wisdomadebola62@gmail.com>
2023-11-18 02:08:16 +0100

    Created Color & Bgcolor For Navbar

--- Zena/tailwind.config.js
+++ Zena/tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          'white-smoke': '#f8f7f3',
+          'linen': '#ede9e4'
+        }
+      },
         fontFamily: {
           heading: ['Sofia Sans', 'sans-serif'],
           text: ['Sofia Sans', 'sans-serif']
